1a8f12804ef96ea682c5d3df055cbc5c56ef6447
Paige Saint Germain <paigejerin@gmail.com>
2025-01-12 19:45:37 -0500

    updated index.js file

--- js/index.js
+++ js/index.js
@@ -1,4 +1,26 @@
-// Prevent us from attempting to use variables
-// that are not declared
-"use strict"
 
+
+// // Prevent us from attempting to use variables
+// // that are not declared
+// "use strict"
+
+// console.log(`Should be 2319 (number interpolated to string) => ${2000 + 300 + 10 + 9}`);
+// console.log(`Should be 2319 (string + string) => ${"23" + "19"}`);
+// console.log(`Should be 22 => ${2 + '2'}`);
+// /*
+// 2 + '2' = 22
+
+// // Modulus
+// console.log(`Should be 0 => ${15 % 5}`);
+// console.log(`Should be 2 => ${20 % 3}`);
+
+// // Comparisons
+// console.log(`Should be true => ${2319 === 2319}`);
+// console.log(`Should be true => ${2319 == '2319'}`);
+// console.log(`Should be false => ${'2319' === 2319}`);
+
+// // Logical
+// console.log(`Should be 3 => ${3 || 2 || 1}`);
+// console.log(`Should be 10 => ${10 || 9 || 8}`);
+// console.log(`Should be false => ${true && false}`);
+// console.log(`Should be true => ${true && true || false}`);
